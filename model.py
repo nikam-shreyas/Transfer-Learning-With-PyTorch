c875dd43b3ed7cd370294a9a9468fc93cb7d7936
ZeroNP <nik4m.5hreyas@gmail.com>
2022-10-16 19:07:38 -0400

    Training Script Done

--- model.py
+++ model.py
@@ -1,13 +1,20 @@
 import torch
 import torch.nn as nn
 
+
 class LeNetModel(nn.Module):
+    """
+    Defining the LeNet Architecture consisting of two Convolutional Layers followed by MaxPooling Layers further
+    connected to two fully connected layers. The activation fucntion used is ReLU activation function, and the model
+    returns the logsoftmax probabilities as the output.
+    """
+
     def __init__(self, input_channels, output_classes):
         super(LeNetModel, self).__init__()
         self.conv1 = nn.Conv2d(input_channels, 20, 5)
         self.maxpool1 = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(20, 50, 5)
-        self.maxpool2 = nn.MaxPool2d(2,2)
+        self.maxpool2 = nn.MaxPool2d(2, 2)
         self.relu = nn.ReLU()
         self.fc1 = nn.Linear(800, 300)
         self.fc2 = nn.Linear(300, output_classes)
